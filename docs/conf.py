727fdeb82c48ed42df02c0e436f64557841a0ba2
Adam Jackman <adam@acjackman.com>
2020-03-09 00:05:21 -0600

    initial project setup

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+"""Sphinx configuration."""
+project = "note-clerk"
+author = "Adam Jackman"
+copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
---
+++ src/note_clerk/console.py
@@ -0,0 +1,11 @@
+"""Note clerk application."""
+import click
+
+from . import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main() -> None:
+    """Note clerk application."""
+    click.echo("Hello, world!")
