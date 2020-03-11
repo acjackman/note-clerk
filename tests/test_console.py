2d7c1cc6a80eba7aa7d85de9a46d71f6193104a0
Adam Jackman <adam@acjackman.com>
2020-03-10 21:52:00 -0600

    🔨 break out the click runner to a fixture

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,18 +4,19 @@ import click.testing
 import note_clerk
 from note_clerk import console
 
+@pytest.fixture
+def cli_runner() -> CliRunner:
+    return click.testing.CliRunner()
 
-def test_main() -> None:
+def test_main(cli_runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    runner = click.testing.CliRunner()
-    result = runner.invoke(console.main)
+    result = cli_runner.invoke(console.main)
     assert result.exit_code == 0
 
 
-def test_version_option() -> None:
+def test_version_option(cli_runner: CliRunner) -> None:
     """Nersion number printed."""
-    runner = click.testing.CliRunner()
-    result = runner.invoke(console.main, "--version")
+    result = cli_runner.invoke(console.main, "--version")
     assert result.exit_code == 0
 
     assert result.output == f"main, version {note_clerk.__version__}\n"
