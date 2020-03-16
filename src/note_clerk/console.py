7738da6945dd5268b0048858531fbd7cbb31e716
Adam Jackman <adam@acjackman.com>
2020-03-16 02:00:48 -0600

    💥 change main to cli

--- src/note_clerk/console.py
+++ src/note_clerk/console.py
@@ -6,6 +6,6 @@ from . import __version__
 
 @click.command()
 @click.version_option(version=__version__)
-def main() -> None:
+def cli() -> None:
     """Note clerk application."""
     click.echo("Hello, world!")
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,22 +1,26 @@
 """note-clerk application tests."""
-import click.testing
+from click.testing import CliRunner
+import pytest
 
 import note_clerk
 from note_clerk import console
 
+
 @pytest.fixture
 def cli_runner() -> CliRunner:
-    return click.testing.CliRunner()
+    """Creates CliRunner instance."""
+    return CliRunner()
+
 
 def test_main(cli_runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = cli_runner.invoke(console.main)
+    result = cli_runner.invoke(console.cli)
     assert result.exit_code == 0
 
 
 def test_version_option(cli_runner: CliRunner) -> None:
     """Nersion number printed."""
-    result = cli_runner.invoke(console.main, "--version")
+    result = cli_runner.invoke(console.cli, "--version")
     assert result.exit_code == 0
 
     assert result.output == f"main, version {note_clerk.__version__}\n"
