657c537240e71acf47f1e02bcc8c52286a7cf6b9
Adam Jackman <adam@acjackman.com>
2021-01-20 23:53:26 -0700

    split out fix code from console code

--- src/note_clerk/console.py
+++ src/note_clerk/console.py
@@ -1,27 +1,21 @@
 """Note clerk application."""
 from dataclasses import dataclass
 from enum import Enum
-from functools import reduce, wraps
-import io
+from functools import reduce
 import json
 import logging
-from pathlib import Path
 import re
 import sys
-from typing import Any, Callable, Dict, Iterable, List, Optional, TextIO, Tuple, TypeVar
+from typing import Callable, Iterable, Optional, TextIO, Tuple, TypeVar
 
-from boltons.fileutils import atomic_save
 import click
 import frontmatter
-from orderedset import OrderedSet
-from ruamel.yaml import YAML
 import yaml
 
 
-from . import __version__, utils
+from . import __version__, fixing, utils
 from .app import App
 from .linting import lint_file
-from .utils import ensure_newline
 
 
 log = logging.getLogger(__name__)
@@ -113,100 +107,12 @@ def lint(ctx: click.Context, app: App, paths: Tuple[str]) -> None:
         ctx.exit(10)
 
 
-class UnableFix(Exception):
-    """Unable to fix this particular file."""
-
-
-def fix_header(header: str) -> str:
-    output = io.StringIO()
-    yaml = YAML(output=output)
-    header_docs = [h for h in yaml.load_all(header) if h is not None]  # noqa: S506
-    log.debug(f"header_docs:\n{header_docs}")
-
-    if len(header_docs) < 2:
-        return header
-
-    combined: Dict[str, Any] = {}
-    for doc in header_docs:
-        log.debug(f"doc:\n{doc}")
-        for key, value in sorted(doc.items(), key=lambda t: t[0]):
-            try:
-                existing = combined[key]
-                if isinstance(existing, list) and isinstance(value, list):
-                    combined[key] = list(OrderedSet(existing + value))
-                elif existing == value:
-                    pass
-                else:
-                    raise UnableFix("Unable to join constants")
-            except KeyError:
-                combined[key] = value
-
-    yaml.dump(combined, output)
-
-    return f"---\n{output.getvalue().strip()}\n***\n"
-
-
-def fix_text(text: TextIO, filename: Optional[str]) -> Tuple[str, Optional[str]]:
-    header, body = utils.split_header([line.strip() for line in text.readlines()])
-    try:
-        new_header = fix_header(header)
-    except Exception:
-        log.error("error creationg header", exc_info=True)
-    log.debug(f"new_header:\n{new_header}")
-    new_note: str = ensure_newline(new_header) + ensure_newline(body.strip())
-    return new_note, filename
-
-
-def raised_error(func: Callable) -> Callable:
-    @wraps(func)
-    def wrapper(*args, **kwargs) -> Iterable[bool]:
-        try:
-            func(*args, **kwargs)
-            yield False
-        except UnableFix:
-            yield True
-
-    return wrapper
-
-
-@raised_error
-def update_text(
-    text: TextIO,
-    filename: Optional[str],
-) -> None:
-    log.debug(f"{filename=}")
-    n_text, n_filename = fix_text(text, filename)
-
-    if n_filename is None:
-        click.echo(n_text.strip())
-    # elif base_path is None:
-    #     raise Exception("base_path must not be none if null filename")
-    else:
-        with atomic_save(n_filename, overwrite=True) as f:
-            f.write(n_text)
-        if filename is not None and filename != n_filename:
-            Path(filename).unlink()
-
-
 @cli.command()
 @click.argument("paths", nargs=-1, type=click.Path())
 @click.pass_obj
 @click.pass_context
 def fix(ctx: click.Context, app: App, paths: Tuple[str]) -> None:
-    log.debug("running fix")
-
-    try:
-        acj: List[bool] = list(
-            _apply_to_paths(
-                paths,
-                update_text,
-            )
-        )
-        log.debug(f"{acj=}")
-    except Exception:
-        log.error("unable to apply to paths", exc_info=True)
-        raise
-    error = reduce(either, acj, False)
+    error = reduce(either, _apply_to_paths(paths, fixing.update_text), False)
     if error:
         ctx.exit(10)
 
---
+++ src/note_clerk/fixing.py
@@ -0,0 +1,92 @@
+from functools import wraps
+import io
+import logging
+from pathlib import Path
+from typing import Any, Callable, Dict, Iterable, Optional, TextIO, Tuple
+
+from boltons.fileutils import atomic_save
+import click
+from orderedset import OrderedSet
+from ruamel.yaml import YAML
+
+
+from . import utils
+from .utils import ensure_newline
+
+
+log = logging.getLogger(__name__)
+
+
+class UnableFix(Exception):
+    """Unable to fix this particular file."""
+
+
+def fix_header(header: str) -> str:
+    output = io.StringIO()
+    yaml = YAML(output=output)
+    header_docs = [h for h in yaml.load_all(header) if h is not None]  # noqa: S506
+    log.debug(f"header_docs:\n{header_docs}")
+
+    if len(header_docs) < 2:
+        return header
+
+    combined: Dict[str, Any] = {}
+    for doc in header_docs:
+        log.debug(f"doc:\n{doc}")
+        for key, value in sorted(doc.items(), key=lambda t: t[0]):
+            try:
+                existing = combined[key]
+                if isinstance(existing, list) and isinstance(value, list):
+                    combined[key] = list(OrderedSet(existing + value))
+                elif existing == value:
+                    pass
+                else:
+                    raise UnableFix("Unable to join constants")
+            except KeyError:
+                combined[key] = value
+
+    yaml.dump(combined, output)
+
+    return f"---\n{output.getvalue().strip()}\n***\n"
+
+
+def fix_text(text: TextIO, filename: Optional[str]) -> Tuple[str, Optional[str]]:
+    header, body = utils.split_header([line.strip() for line in text.readlines()])
+    try:
+        new_header = fix_header(header)
+    except Exception:
+        log.error("error creationg header", exc_info=True)
+    log.debug(f"new_header:\n{new_header}")
+    new_note: str = ensure_newline(new_header) + ensure_newline(body.strip())
+    return new_note, filename
+
+
+def raised_error(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Iterable[bool]:
+        try:
+            func(*args, **kwargs)
+            yield False
+        except UnableFix:
+            yield True
+
+    return wrapper
+
+
+@raised_error
+def update_text(
+    text: TextIO,
+    filename: Optional[str],
+) -> None:
+    log.debug(f"{filename=}")
+    n_text, n_filename = fix_text(text, filename)
+
+    if n_filename is None:
+        click.echo(n_text.strip())
+    # elif base_path is None:
+    #     raise Exception("base_path must not be none if null filename")
+    else:
+        with atomic_save(n_filename, overwrite=True) as f:
+            f.write(n_text)
+        if filename is not None and filename != n_filename:
+            Path(filename).unlink()
