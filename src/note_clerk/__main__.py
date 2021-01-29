5c8d89bce89833f632974d77b95d36537b92b2a8
Adam Jackman <adam@acjackman.com>
2021-01-29 02:18:47 -0700

    skip coverage of main

--- src/note_clerk/__main__.py
+++ src/note_clerk/__main__.py
@@ -1,5 +1,5 @@
-from .console import cli
+from .console import cli  # pragma: no cover
 
 
-if __name__ == "__main__":
-    cli(prog_name="note-clerk")  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
+    cli(prog_name="note-clerk")
