eedfd200dbf48ba566e8077b60fa451e0178002c
Adam Jackman <adam@acjackman.com>
2021-03-22 02:20:26 -0600

    Plan week adjustable date (#54)

--- src/note_clerk/planning.py
+++ src/note_clerk/planning.py
@@ -6,9 +6,10 @@ from typing import Optional
 from note_clerk import utils
 
 
-def day_link(date: dt.datetime, days: int = 0, link_fmt: str = "%A %Y-%m-%d") -> str:
+def day_link(date: dt.datetime, days: int = 0, link_fmt: str = "%Y-%m-%d") -> str:
     d = date + dt.timedelta(days=days)
-    return f"[[{d:%Y%m%d}060000|{d:link_fmt}]]"
+    formatted = d.strftime(link_fmt)
+    return f"[[{d:%Y%m%d}060000|{formatted}]]"
 
 
 def week_label(date: dt.datetime) -> str:
@@ -28,7 +29,7 @@ def quarter_link(date: dt.datetime, link_fmt: str = "%A %Y-%m-%d") -> str:
 
 
 def generate_week_plan(date: dt.datetime) -> str:
-    day = partial(day_link, date)
+    day = partial(day_link, date, link_fmt="%A %Y-%m-%d")
 
     week = week_label(date)
     week_num = date.isocalendar()[1]
@@ -50,6 +51,7 @@ def generate_week_plan(date: dt.datetime) -> str:
         ### Habits
 
         ## Action Plans
+        - {day(0)}
         - {day(1)}
         - {day(2)}
         - {day(3)}
@@ -102,12 +104,24 @@ def create_day_plan_file(date: dt.datetime, note_dir: Path) -> Path:
 
 
 def quarter_start(date: Optional[dt.datetime] = None) -> dt.datetime:
-    date = date or dt.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    date = date or dt.datetime.now()
     month = date.month - ((date.month - 1) % 3)
     return date.replace(month=month, day=1, hour=0, minute=0, second=0, microsecond=0)
 
 
 def last_monday(date: Optional[dt.datetime] = None) -> dt.datetime:
-    date = date or dt.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    date = date or dt.datetime.now()
     monday = date - dt.timedelta(days=date.weekday())
     return monday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def determine_date(
+    date: dt.datetime, _next: bool = False, _prev: bool = False
+) -> dt.datetime:
+    if _next and _prev:
+        raise Exception("--next and --prev must not be passed together")
+    elif _next:
+        date += dt.timedelta(days=1)
+    elif _prev:
+        date -= dt.timedelta(days=1)
+    return date
